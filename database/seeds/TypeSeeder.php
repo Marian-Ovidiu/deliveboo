d96573f1a2b6f730bd7dfb037bd60e849722a7e3
francesco-maurella <francesco.maurella@live.it>
2021-04-14 16:01:04 +0200

    update

--- database/seeds/TypeSeeder.php
+++ database/seeds/TypeSeeder.php
@@ -58,14 +58,19 @@ class TypeSeeder extends Seeder
         $allTypes = Type::all();
 
         foreach($businesses as $business){
+          $nums = [];
             for($i = 0; $i < rand(1, 3); $i++){
-                $business->types()->save($allTypes[rand(1, 12)]);
+              $business->types()->save($allTypes[$this->uniqueId(1, 12, $nums)]);
             }
         }
-
-
-
-
-
     }
+
+    // Funzione di assegnazione id univoco relativo a Types
+    public function uniqueId($min, $max, $nums) {
+        $num = rand ($min, $max);
+        if(!in_array($num, $nums)) {
+          $nums[] = $num;
+        }
+        return $num;
+      }
 }
