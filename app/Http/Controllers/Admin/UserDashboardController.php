f47cf9f964b870e452fdf8dde2a32829996ec6f8
Marian-Ovidiu <h.marian914@gmail.com>
2021-04-16 15:37:52 +0200

    update

--- app/Http/Controllers/Admin/UserDashboardController.php
+++ app/Http/Controllers/Admin/UserDashboardController.php
@@ -9,7 +9,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class DashboardController extends Controller
+class UserDashboardController extends Controller
 {
     public function index(){
       /*
