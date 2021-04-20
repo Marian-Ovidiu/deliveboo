9542b458b69f826a84a27e16685082aa529b8d56
Marian-Ovidiu <h.marian914@gmail.com>
2021-04-20 15:25:33 +0200

    updated home graphics

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -42,32 +42,38 @@
                     </div>
                     <div class="col-2 row-jumbotronn-row-space fl"></div>
                 </div>
-
-                <div class="row-jumbotronn-row row">
-                    <div class="col-2 row-jumbotronn-row-space fl"></div>
-                    <div class="col-8 row-jumbotronn-row-types fl">
-                        <div class="row-jumbotronn-row-types-row row">
-
-                            <div class="col-3 row-jumbotronn-row-types-row-type" v-for="(type, i) in types">
-                                <div class="row-jumbotronn-row-types-row-type-t">
-                                    <a href="#restaurants" v-on:click="filterBusinessesByTypes(type.name)">
-                                        <img alt="type.name" v-bind:src="type.img" class="rounded" width="200" height="100">
-                                        <h6>
-                                            <span>@{{ type.name }}</span>
-                                        </h6>
-                                    </a>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                    <div class="col-2 row-jumbotronn-row-space fl"></div>
-                </div>
-
             </div>
         </div>
 
         <div class="row">
+            <div class="col-12 row-types">
+                <div class="row-types-row row">
+                    <div class="col-12 row-types-row-title fl">
+                        Scegli per categoria
+                    </div>
+                </div>
+                <div class="row-types-row row">
+                    <div class="col-2 row-types-row-space fl"></div>
+                    <div class="col-8 row-types-row-types fl">
+                        <div class="row-types-row-types-row row">
+                            <div class="col-3 row-types-row-types-row-type" v-for="(type, i) in types">
+                                <div class="row-types-row-types-row-type-t">
+                                    <a href="#restaurants-row" v-on:click="filterBusinessesByTypes(type.name)">
+                                        <img alt="type.name" v-bind:src="type.img" class="rounded" width="200" height="100">
+                                        <h6>
+                                            <span style="color:black;">@{{ type.name }}</span>
+                                        </h6>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-2 row-types-row-space fl"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row" id="restaurants-row">
             <div class="col-12 row-restaurants">
                 <div class="row-restaurants-row row">
                     <div class="col-12 row-restaurants-row-title">Choose From Most Popular</div>
@@ -76,55 +82,58 @@
                     <div class="col-12 row-restaurants-row-subtitle">All the top restaurant in your city</div>
                 </div>
                 <div id="restaurants" data-spy="restaurants" data-target="#restaurants" class="row-restaurants-row row">
-                    <div class="col-2 row-restaurants-row-space fl"></div>
-                    <div class="col-8 row-restaurants-row-cards fl">
-                        <div class="row-restaurants-row-cards-row row">
+                    <div class="col-1 row-restaurants-row-space fl"></div>
+                    <div class="col-10 row-restaurants-row-cards fl">
 
-                            <div class="col-4 row-restaurants-row-cards-row-card"
-                                v-for="(business, i) in businesses"
-                                v-bind:key="i"
-                                v-if="searchFunction(business.name)">
-                                <div class="row-restaurants-row-cards-row-card-img">
-                                    <img :src="business.logo" alt="business.name" :value="business.id">
-                                </div>
-                                <div class="row-restaurants-row-cards-row-card-body">
-                                    <div class="row-restaurants-row-cards-row-card-body-name">
-                                        @{{ business.name }}
-                                    </div>
-                                    <div class="row-restaurants-row-cards-row-card-body-type">
-                                        @{{ business.description }}
-                                    </div>
-                                    <div class="row-restaurants-row-cards-row-card-body-address">
-                                        <i class="fa fa-map-marker" aria-hidden="true"></i>
-                                        @{{ business.address }}
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-4 row-restaurants-row-cards-row-card"
-                            v-for="(business, i) in businessesForType"
+                        <div class="row-restaurants-row-cards-card"
+                            v-for="(business, i) in businesses"
                             v-bind:key="i"
-                            v-if="businessesForType.length > 0">
-                                <div class="row-restaurants-row-cards-row-card-img">
-                                    <img :src="business.logo" alt="business.name" :value="business.id">
+                            v-if="searchFunction(business.name)">
+                            <div class="row-restaurants-row-cards-card-img" :style="{ 'background-image': 'url(' + business.logo + ')' }"></div>
+                            <div class="row-restaurants-row-cards-card-body">
+                                <div class="row-restaurants-row-cards-card-body-name">
+                                    @{{ business.name }}
                                 </div>
-                                <div class="row-restaurants-row-cards-row-card-body">
-                                    <div class="row-restaurants-row-cards-row-card-body-name">
-                                        @{{ business.name }}
-                                    </div>
-                                    <div class="row-restaurants-row-cards-row-card-body-type">
-                                        @{{ business.description }}
-                                    </div>
-                                    <div class="row-restaurants-row-cards-row-card-body-address">
-                                        <i class="fa fa-map-marker" aria-hidden="true"></i>
-                                        @{{ business.address }}
-                                    </div>
+                                <div class="row-restaurants-row-cards-card-body-description">
+                                    @{{ business.description }}
+                                  </div>
+                                  <div class="row-restaurants-row-cards-card-body-title">Orari</div>
+                                <div class="row-restaurants-row-cards-card-body-hours">
+                                  <span>Dalle: @{{ business.opening_time }}</span><br>
+                                  <span>Alle: @{{ business.closing_time }}</span>
+                                </div>
+                                <div class="row-restaurants-row-cards-card-body-address">
+                                    <i class="fas fa-map-marker-alt"></i>
+                                    @{{ business.address }}
                                 </div>
                             </div>
-
                         </div>
+
+                        <div class="row-restaurants-row-cards-card"
+                        v-for="(business, i) in businessesForType"
+                        v-bind:key="i"
+                        v-if="businessesForType.length > 0">
+                            <div class="row-restaurants-row-cards-card-img" :style="{ 'background-image': 'url(' + business.logo + ')' }"></div>
+                            <div class="row-restaurants-row-cards-card-body">
+                                <div class="row-restaurants-row-cards-card-body-name">
+                                    @{{ business.name }}
+                                </div>
+                                <div class="row-restaurants-row-cards-card-body-title">Orari</div>
+                                <div class="row-restaurants-row-cards-card-body-hours">
+                                    @{{ business.opening_time }}
+                                </div>
+                                <div class="row-restaurants-row-cards-card-body-hours">
+                                    @{{ business.closing_time }}
+                                </div>
+                                <div class="row-restaurants-row-cards-card-body-address">
+                                    <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                    @{{ business.address }}
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
-                    <div class="col-2 row-restaurants-row-space fl"></div>
+                    <div class="col-1 row-restaurants-row-space fl"></div>
                 </div>
             </div>
         </div>
