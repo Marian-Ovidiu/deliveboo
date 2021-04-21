4c94fc04a99944bc254cb6a215aeec2a3b190a62
mrota89 <marcello.rota.1989@gmail.com>
2021-04-21 17:26:44 +0200

    prova braintree

--- app/Http/Controllers/Guest/OrderController.php
+++ app/Http/Controllers/Guest/OrderController.php
@@ -1,22 +1,32 @@
 <?php
 
 namespace App\Http\Controllers\Guest;
-
+use Braintree\Gateway as Gateway;
+use Braintree\Transaction as Transaction;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Business;
-use App\Product;
 use App\Order;
 
 class OrderController extends Controller
 {
     public function checkout(Business $business)
     {
-        return view('guest.checkout', compact('business'));
+
+        $gateway = new Gateway([
+        'environment' => 'sandbox',
+        'merchantId' => 'nyfdp2wz77gqqj29',
+        'publicKey' => '8bdc65hxyy4pg56f',
+        'privateKey' => 'e16fd716976555b304d8b2d18ad5ce55'
+        ]);
+
+        $token = $gateway->ClientToken()->generate();
+        return view('guest.checkout', compact('business', 'token', 'gateway'));
     }
 
-    public function store(Request $request, Product $products)
+    public function store(Request $request)
     {
+
       $data = $request->all();
 
       $products= [];
@@ -27,10 +37,38 @@ class OrderController extends Controller
         }
       }
 
+
       $order = new Order();
       $order->fill($data);
+      $gateway = new Gateway([
+        'environment' => 'sandbox',
+        'merchantId' => 'nyfdp2wz77gqqj29',
+        'publicKey' => '8bdc65hxyy4pg56f',
+        'privateKey' => 'e16fd716976555b304d8b2d18ad5ce55'
+        ]);
       $order->save();
       $order->products()->attach($products);
+
+        $nonce = true;
+
+        $result = $gateway->transaction()->sale([
+        'amount' => $order->amount,
+        'paymentMethodNonce' => $nonce,
+        'options' => [
+            'submitForSettlement' => true
+            ]
+        ]);
+        dd($result);
+        if ($result->success) {
+            $transaction = $result->transaction;
+            return redirect()->route('purchase-made', ['transaction'=>$transaction,'order'=>$order]);
+        } else {
+            $errorString = "";
+
+            foreach ($result->errors->deepAll() as $error) {
+                $errorString .= 'Error: ' . $error->code . ": " . $error->message . "\n";
+            }
+        }
 
       return view('guest.success');
     }
--- resources/views/guest/checkout.blade.php
+++ resources/views/guest/checkout.blade.php
@@ -104,4 +104,38 @@
     <input v-for ="product in cartSaved" type = "hidden" name = "quantities[]" :value = "product.quantity"/>
 
     <input type="submit" value="Procedi all'ordine">
+
+    <script src="https://js.braintreegateway.com/web/dropin/1.13.0/js/dropin.min.js"></script>
+      <script>
+          var form = document.querySelector('#payment-form');
+          var client_token = "{{ $token }}";
+          var onBox = $("#loading");
+          onBox.addClass("not-active")
+          braintree.dropin.create({
+            authorization: client_token,
+            selector: '#bt-dropin',
+            paypal: {
+              flow: 'vault'
+            }
+          }, function (createErr, instance) {
+            if (createErr) {
+              console.log('Create Error', createErr);
+              return;
+            }
+            form.addEventListener('submit', function (event) {
+              onBox.removeClass('not-active').addClass('active-container').fadeIn('slow');
+              event.preventDefault();
+              instance.requestPaymentMethod(function (err, payload) {
+                if (err) {
+                  onBox.removeClass('active-container').addClass('not-active').fadeIn('slow');
+                  console.log('Request Payment Method Error', err);
+                  return;
+                }
+                // Add the nonce to the form and submit
+                document.querySelector('#nonce').value = payload.nonce;
+                form.submit();
+              });
+            });
+          });
+      </script>
   </form>
