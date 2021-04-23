41acaf5ca579d6a0daab31a13730d8caa2ac6c9e
francesco-maurella <francesco.maurella@live.it>
2021-04-23 12:43:35 +0200

    logo

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,27 +1,50 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    {{-- vuejs --}}
-    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.js"></script>
-    {{-- style --}}
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    {{-- javascript --}}
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <title>Deliveboo - @yield('title')</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  {{-- Vuejs --}}
+  <script src="https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.js"></script>
+  {{-- Style --}}
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  {{-- Script --}}
+  <script src="{{ asset('js/app.js') }}" defer></script>
+  <title>Deliveboo - @yield('title')</title>
 </head>
 <body>
+  {{-- WRAPPER CONTAINER --}}
+  <div class="wrapper">
 
-    <div class="wrapper">
+    {{-- Header --}}
+    <header class="top-header">
+      {{-- head-logo --}}
+      <div class="top-header-logo">
+        <img src="{{ asset('img/00-logo.jpg') }}" alt="Deliveboo">
+      </div>
+      {{-- / head-logo --}}
+      {{-- head-menu-LG --}}
+      <nav class="top-header-menu">
+        <ul>
+          <li><a href="{{ route('public-home') }}"><span>Home</span></a></li>
+          @auth
+            <li><a href="{{ route('dashboard') }}">Area ristoratore [{{ Auth::user()->name }}]</a></li>
+          @else
+            <li><a href="{{ route('login') }}">Login</a></li>
+            <li><a href="{{ route('register') }}">Registrati</a></li>
+          @endauth
+        </ul>
+      </nav>
+      {{-- / head-menu-LG --}}
+    </header>
+    {{-- / Header --}}
+    @yield('content')
+    @include('parts.footer')
 
-        @include('parts.navbar')
-        @yield('content')
-        @include('parts.footer')
-
-    </div>
+  </div>
+  {{-- / WRAPPER CONTAINER --}}
 
 
 </body>
