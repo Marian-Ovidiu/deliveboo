3ebc0776b1e679bde61798f1867046f8c6d28648
Marian-Ovidiu <h.marian914@gmail.com>
2021-04-23 20:30:54 +0200

    update

--- resources/views/guest/order-summary.blade.php
+++ resources/views/guest/order-summary.blade.php
@@ -34,7 +34,7 @@
                             {{-- row: name --}}
                             <div class="form-group">
                                 <label for="customer_name"><b>Nome</b></label>
-                                <input type="text" class="form-control {{ $errors->has('customer_name') ? 'is-invalid' : '' }}" id="customer_name" name="customer_name">
+                                <input type="text" class="form-control {{ $errors->has('customer_name') ? 'is-invalid' : '' }}" id="customer_name" name="customer_name" placeholder="Nome">
                                 <div class="invalid-feedback">
                                     {{ $errors->first('name') }}
                                 </div>
@@ -45,9 +45,9 @@
                             {{-- row: last name --}}
                             <div class="form-group">
                                 <label for="customer_last_name"><b>Cognome</b></label>
-                                <input type="text" class="form-control {{ $errors->has('customer_last_name') ? 'is-invalid' : '' }}" id="customer_last_name" name="customer_last_name">
+                                <input type="text" class="form-control {{ $errors->has('customer_last') ? 'is-invalid' : '' }}" id="customer_last_name" name="customer_last_name" placeholder="Cognome">
                                 <div class="invalid-feedback">
-                                    {{ $errors->first('last_name') }}
+                                    {{ $errors->first('customer_last') }}
                                 </div>
                             </div>
                             {{-- / row: last name --}}
@@ -58,7 +58,7 @@
                             {{-- row: city --}}
                             <div class="form-group">
                                 <label for="city"><b>Città</b></label>
-                                <input type="text" class="form-control {{ $errors->has('city') ? 'is-invalid' : '' }}" id="city" name="city">
+                                <input type="text" class="form-control {{ $errors->has('city') ? 'is-invalid' : '' }}" id="city" name="city" placeholder="Città">
                                 <div class="invalid-feedback">
                                     {{ $errors->first('city') }}
                                 </div>
@@ -68,55 +68,69 @@
                         <div class="col-2">
                             {{-- row: postal_code --}}
                             <div class="form-group">
-                                <label for="customer_address"><b>Numero</b></label>
-                                <input type="text" class="form-control {{ $errors->has('customer_address') ? 'is-invalid' : '' }}" id="customer_address_number" name="customer_address_number">
+                                <label for="postal_code"><b>Cap</b></label>
+                                <input type="text" class="form-control {{ $errors->has('postal_code') ? 'is-invalid' : '' }}" id="postal_code" name="postal_code" placeholder="CAP">
                                 <div class="invalid-feedback">
-                                    {{ $errors->first('address') }}
+                                    {{ $errors->first('postal_code') }}
                                 </div>
                             </div>
                             {{-- / row: postal_code --}}
                         </div>
                     </div>
 
-                     {{-- row: address --}}
-                     <div class="form-group">
-                        <label for="customer_address"><b>Numero</b></label>
-                        <input type="text" class="form-control {{ $errors->has('customer_address') ? 'is-invalid' : '' }}" id="customer_address_number" name="customer_address_number">
-                        <div class="invalid-feedback">
-                            {{ $errors->first('address') }}
+                    <div class="row">
+                        <div class="col-12">
+                            {{-- row: address --}}
+                            <div class="form-group">
+                                <label for="customer_address"><b>Indirizzo</b></label>
+                                <input type="text" class="form-control {{ $errors->has('address') ? 'is-invalid' : '' }}" id="customer_address_number" name="customer_address_number" placeholder="Indirizzo">
+                                <div class="invalid-feedback">
+                                    {{ $errors->first('address') }}
+                                </div>
+                            </div>
+                            {{-- / row: address --}}
                         </div>
                     </div>
-                    {{-- / row: address --}}
 
-                    {{-- row: email --}}
-                    <div class="form-group">
-                    <label for="customer_email"><b>Email</b></label>
-                    <input type="text" class="form-control {{ $errors->has('customer_email') ? 'is-invalid' : '' }}" id="customer_email" name="customer_email">
-                    <div class="invalid-feedback">
-                        {{ $errors->first('email') }}
-                    </div>
-                    </div>
-                    {{-- / row: email --}}
 
-                    {{-- row: telephone --}}
-                    <div class="form-group">
-                    <label for="customer_telephone"><b>Telefono</b></label>
-                    <input type="text" class="form-control {{ $errors->has('customer_telephone') ? 'is-invalid' : '' }}" id="customer_telephone" name="customer_telephone">
-                    <div class="invalid-feedback">
-                        {{ $errors->first('telephone') }}
+                    <div class="row">
+                        <div class="col-6">
+                            {{-- row: email --}}
+                            <div class="form-group">
+                                <label for="customer_email"><b>Email</b></label>
+                                <input type="text" class="form-control {{ $errors->has('customer_email') ? 'is-invalid' : '' }}" id="customer_email" name="customer_email" placeholder="Email">
+                                <div class="invalid-feedback">
+                                    {{ $errors->first('email') }}
+                                </div>
+                            </div>
+                            {{-- / row: email --}}
+                        </div>
+                        <div class="col-6">
+                            {{-- row: telephone --}}
+                            <div class="form-group">
+                                <label for="customer_telephone"><b>Telefono</b></label>
+                                <input type="text" class="form-control {{ $errors->has('customer_telephone') ? 'is-invalid' : '' }}" id="customer_telephone" name="customer_telephone" placeholder="Numero di Telefono">
+                                <div class="invalid-feedback">
+                                    {{ $errors->first('telephone') }}
+                                </div>
+                            </div>
+                            {{-- / row: telephone --}}
+                        </div>
                     </div>
-                    </div>
-                    {{-- / row: telephone --}}
 
-                    {{-- row: notes --}}
-                    <div class="form-group">
-                    <label for="notes"><b>Annotazioni (facoltativo)</b></label>
-                    <input type="text" class="form-control {{ $errors->has('notes') ? 'is-invalid' : '' }}" id="notes" name="notes">
-                    <div class="invalid-feedback">
-                        {{ $errors->first('notes') }}
+                    <div class="row">
+                        <div class="col-12">
+                            {{-- row: notes --}}
+                            <div class="form-group">
+                                <label for="notes"><b>Annotazioni (facoltativo)</b></label>
+                                <textarea class="form-control {{ $errors->has('notes') ? 'is-invalid' : '' }}" id="notes" name="notes" placeholder="(facoltativo)"></textarea>
+                                <div class="invalid-feedback">
+                                {{ $errors->first('notes') }}
+                            </div>
+                        </div>
+                        {{-- / row: notes --}}
+                        </div>
                     </div>
-                    </div>
-                    {{-- / row: notes --}}
 
                     {{-- row: success --}}
                     <input type="hidden" value="1" name="success">
@@ -126,20 +140,14 @@
                     <input type="hidden" :value="amount" name="amount">
                     {{-- / row: amount --}}
 
-                    <br>
-                    <br>
-                    <hr>
-                    <br>
-                    <br>
-
                     <input v-for ="product in cartSaved" type = "hidden" name = "products[]" :value = "product.id"/>
                     <input v-for ="product in cartSaved" type = "hidden" name = "quantities[]" :value = "product.quantity"/>
-
-                    <input type="submit" value="Procedi all'ordine">
 
                     <div class="bt-drop-in-wrapper">
                     <div id="bt-dropin"></div>
                     </div>
+
+                    <input type="submit" value="Procedi all'ordine" style="margin: 10px 0; width: 300px; padding: 15px; color: white; background-color: #0389ff; border: none;">
                 </div>
 
             </form>
