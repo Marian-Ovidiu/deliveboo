77f272493aca9ee8f780ebbe1c429c0a8fe2bc3c
francesco-maurella <francesco.maurella@live.it>
2021-04-23 15:00:30 +0200

    Refactoring #2.5 - LOGO ANIMATO

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,33 +1,40 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- CSRF Token -->
+  {{-- CSRF Token --}}
   <meta name="csrf-token" content="{{ csrf_token() }}">
+
   {{-- Vuejs --}}
   <script src="https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.js"></script>
+
   {{-- Style --}}
   <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
   {{-- Script --}}
   <script src="{{ asset('js/app.js') }}" defer></script>
+
   <title>Deliveboo - @yield('title')</title>
 </head>
+
 <body>
+
   {{-- WRAPPER CONTAINER --}}
   <div id="app" class="wrapper">
 
     {{-- Header --}}
     <header class="top-header">
+
       {{-- head-logo --}}
       <div class="top-header-logo">
-        <img src="{{ asset('img/00-logo.jpg') }}" alt="Deliveboo">
+        <img src="{{ asset('img/00-logo.gif') }}" alt="Deliveboo">
       </div>
       {{-- / head-logo --}}
+
       {{-- head-menu-SM --}}
       <nav class="top-header-menu sm">
-          <i @click="viewHambMenu()" class="fas fa-bars" v-if="!hambMenu"></i>
+        <i @click="viewHambMenu()" class="fas fa-bars" v-if="!hambMenu"></i>
         <ul class="top-header-menu-sm" v-if="hambMenu">
           <li>
             <i @click="viewHambMenu()" class="fas fa-times"></i>
@@ -42,6 +49,7 @@
         </ul>
       </nav>
       {{-- / head-menu-SM --}}
+
       {{-- head-menu-LG --}}
       <nav class="top-header-menu lg">
         <ul class="top-header-menu-lg">
@@ -57,8 +65,11 @@
       {{-- / head-menu-LG --}}
     </header>
     {{-- / Header --}}
+
+    {{-- Main content --}}
     @yield('content')
     @include('parts.footer')
+    {{-- / Main content --}}
 
   </div>
   {{-- / WRAPPER CONTAINER --}}
