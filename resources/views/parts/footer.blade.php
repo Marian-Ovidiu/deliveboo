ee48eee62d9c2f518f5cb8eb342ca7ea7558e78d
Marian-Ovidiu <h.marian914@gmail.com>
2021-04-24 13:03:24 +0200

    refactoring header buisiness and footer

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -1,10 +1,10 @@
 <footer>
   <div class="row">
-    <div class="col-xl-2 col-lg-2 col-md-1 col-sm-0 col-3 row-space"></div>
-    <div  class="col-xl-8 col-lg-8 col-md-10 col-sm-12 col-6 row-body">
+    <div class="col-xl-2 col-lg-2 col-md-1 col-sm-1 col-2 row-space"></div>
+    <div  class="col-xl-8 col-lg-8 col-md-10 col-sm-10 col-8 row-body">
       <div class="row-body-row row">
 
-        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12 row-body-row-card">
+        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12 row-body-row-card">
           <div class="row-body-row-card-title">About Us</div>
           <div class="row-body-row-card-text">Lorem ipsum dolor sit amet,
             consectetuer adipiscing elit.Donec
@@ -16,7 +16,7 @@
           </div>
         </div>
 
-        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-6 row-body-row-card" style="padding-left: 50px;">
+        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12 row-body-row-card">
           <div class="row-body-row-card-title">Popular Cities</div>
           <ul class="row-body-row-card-ul">
             <li>Torino</li>
@@ -29,7 +29,7 @@
           </ul>
         </div>
 
-        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 row-body-row-card">
+        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 row-body-row-card">
           <div class="row-body-row-card-title">Connect Us</div>
           <div class="row-body-row-card-box">
             <div class="row-body-row-card-box-img fl">
@@ -53,6 +53,6 @@
         </div>
       </div>
     </div>
-    <div class="col-xl-2 col-lg-2 col-md-1 col-sm-0 col-3 row-space"></div>
+    <div class="col-xl-2 col-lg-2 col-md-1 col-sm-1 col-2 row-space"></div>
   </div>
 </footer>
