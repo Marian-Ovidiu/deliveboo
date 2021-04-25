a40f77cab8115f1ebb51035ff0c2756419db7c9e
mrota89 <marcello.rota.1989@gmail.com>
2021-04-25 14:26:55 +0200

    aggiornate view coferma/rifiuto matodo pagamento

--- resources/views/guest/order-error.blade.php
+++ resources/views/guest/order-error.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.base')
+@section('title', 'Pagamento rifiutato')
 
 @section('content')
-  @dump($errors)
+<div class="row-jumbotron">
+    <div class="row content">
+      <div class="card border-dark mb-3" style="width: 100%;">
+          <div class="card-body text-dark"style="text-align: center;" >
+              <h5 class="card-title">Pagamento rifiutato!</h5>
+              <p class="card-text">Torna alla pagina precedente, verifica i dati inseriti e riprova.</p>
+          </div>
+          <a href="{{ route('cart-checkout') }}" class="btn btn-danger">Torna indietro</a>
+      </div>
+    </div>
+</div>  
 @endsection
